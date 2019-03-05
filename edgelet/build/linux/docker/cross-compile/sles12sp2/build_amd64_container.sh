7ebf70367fa1f8031a832ad5de4f2cb11a0f97ff
Maximilian Murauer <maximilian.murauer@bmwiti.onmicrosoft.com>
2019-03-05 18:04:20 +0100

    Added Sles Files

---
+++ edgelet/build/linux/docker/cross-compile/sles12sp2/build_amd64_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+. ./containers.sh
+
+main() {
+    local Dockerfile.sles12sp2.x86_64-unknown-linux-gnu
+    if [[ ! -f $dockerfile ]]
+    then
+        echo "Expected $dockerfile in current directory."
+        return 1
+    fi
+
+    build_container $dockerfile $CONTAINER_REGISTRY/sles-build:12.sp2-1
+}
+
+main "${@}"
